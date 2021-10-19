d16d37c68dc62a09a75b6e7eee7c52761e398081
Glenn Hermans <glhermans@gmail.com>
2021-10-20 01:47:05 +0200

    Added service providers

---
+++ src/Providers/ModuleServiceProvider.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Bagisto\StockNotify\Providers;
+
+use Konekt\Concord\BaseModuleServiceProvider;
+
+class ModuleServiceProvider extends BaseModuleServiceProvider
+{
+    protected $models = [
+    ];
+}
---
+++ src/Providers/StockNotifyServiceProvider.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace Bagisto\StockNotify\Providers;
+
+use Bagisto\StockNotify\Console\Commands\SendOutOfStockNotifcation;
+
+use Illuminate\Support\ServiceProvider;
+use Illuminate\Console\Scheduling\Schedule;
+use Illuminate\Support\Facades\Event;
+
+class StockNotifyServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+
+        $this->loadTranslationsFrom(__DIR__ . '/../Resources/lang', 'stocknotify');
+
+        $this->loadViewsFrom(__DIR__ . '/../Resources/views', 'stocknotify');
+
+        $this->app->booted(function () {
+            $schedule = $this->app->make(Schedule::class);
+
+            if (core()->getConfigData('catalog.inventory.notifications.status')) {
+                if (core()->getConfigData('catalog.inventory.notifications.schedule') == "hourly") {
+                    $schedule->command(SendOutOfStockNotifcation::class)->hourly()->runInBackground();
+                }
+
+                if (core()->getConfigData('catalog.inventory.notifications.schedule') == "daily") {
+                    $schedule->command(SendOutOfStockNotifcation::class)->daily()->runInBackground();
+                }
+
+                if (core()->getConfigData('catalog.inventory.notifications.schedule') == "weekly") {
+                    $schedule->command(SendOutOfStockNotifcation::class)->weekly()->runInBackground();
+                }
+
+                if ($this->app->runningInConsole()) {
+                    $this->commands([
+                        SendOutOfStockNotifcation::class
+                    ]);
+                }
+            }
+
+        });
+    }
+
+    /**
+     * Register services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        $this->registerConfig();
+    }
+
+    /**
+     * Register package config.
+     *
+     * @return void
+     */
+    protected function registerConfig()
+    {
+        $this->mergeConfigFrom(
+            dirname(__DIR__) . '/Config/system.php', 'core'
+        );
+    }
+}
